22ae8130ca97df706b00ae6a987ffa029a6d9c36
NeoAtlantis <rijndael@yeah.net>
2013-11-19 22:26:43 +0800

    init

--- reception.js
+++ reception.js
@@ -4,4 +4,19 @@
  * This is the start up script for Reception Server, part of the kernel.
  */
 
+require('./lib/baum.js');
+CONFIG = $.config.createConfig('./config/');
 
+var port = CONFIG.get('http-port');
+
+var HTTPServer = $.net.HTTP.server(port);
+console.log('HTTP Server created at port: ' + port);
+
+HTTPServer.on('data', function(e){
+    e.response.setHeader('content-type', 'text/html');
+    e.response.writeHead(200);
+    e.response.write('hello');
+    e.end('Here is Reception from Project Loesung.');
+});
+
+HTTPServer.start();
