f583d78fbbf3e3e5c6cfbe5cee94e552f9f474f2
NeoAtlantis <rijndael@yeah.net>
2013-11-20 00:22:09 +0800

    update

--- reception.js
+++ reception.js
@@ -7,16 +7,41 @@
 require('./lib/baum.js');
 CONFIG = $.config.createConfig('./config/');
 
-var port = CONFIG.get('http-port');
+function errorWriter(e, code){
+    var output
+        = '<html><head><title>' 
+        + CONFIG.get('site-name') 
+        + '</title></head><body>'
+        + '<h3>' + code + ' ' + $.nodejs.http.STATUS_CODES[code] + '</h3>'
+        + '</body></html>'
+    ;
+    e.response.writeHead(code);
+    e.response.write(output);
+    e.response.end();
+};
 
+var routerTable = {
+    
+};
+
+var port = CONFIG.get('http-port');
 var HTTPServer = $.net.HTTP.server(port);
 console.log('HTTP Server created at port: ' + port);
 
 HTTPServer.on('data', function(e){
-    e.response.setHeader('content-type', 'text/html');
-    e.response.writeHead(200);
-    e.response.write('hello');
-    e.end('Here is Reception from Project Loesung.');
+    var handled = false;
+    for(var expression in routerTable){
+        var regexp = new RegExp(expression);
+        var result = regexp.exec(e.response.url);
+        if(null != result){
+            routerTable[expression](e, result);
+            handled = true;
+        };
+    };
+
+    if(!handled){
+        errorWriter(e, 404);
+    };
 });
 
 HTTPServer.start();
