7a55ed84a1b7cc629483082af6b84ec920234617
NeoAtlantis <rijndael@yeah.net>
2013-11-21 02:38:52 +0800

    able to show something in table

--- site/page.monitor.js
+++ site/page.monitor.js
@@ -25,22 +25,65 @@ module.exports = function(e, matchResult){
         if(false == states){
             content = '在查询中出现错误。';
         } else {
-            var translation = {
-                'regtable': '注册表管理器',
+            var list = [
+                ['regtable', '注册表管理器', '管理系统各部件配置参数的数据库，为系统部件提供数据。'],
+                ['crypt',    '加密服务',     '提供应用对称加密算法进行加密和解密的服务。'],
+            ];
+
+            content += '<table class="report" cellspacing="0px" cellpadding="3px">'
+                + '<tr class="head">'
+                  + '<td width="20%">项目</td>'
+                  + '<td width="15%">状态</td>'
+                  + '<td>解释</td>'
+                + '</tr>'
+            ;
+            function showState(what){
+                var className = '', desc = '';
+                switch(what){
+                    case true:
+                        className = 'good';
+                        desc = '正常运行';
+                        break;
+                    case false:
+                        className = 'error';
+                        desc = '无法连通';
+                        break;
+                    default:
+                        className = 'unknow';
+                        desc = '未知';
+                        break;
+                };
+                return '<td class="switch ' + className + '">' + desc + '</td>';
             };
-            for(var each in states){
-                content += translation[each]
-                    + '当前状况'
-                    + (states[each]? '良好': '错误')
-                    + '<br />'
+            for(var each in list){
+                content += '<tr>'
+                content += '<td>' + list[each][1] + '</td>'
+                    + showState(states[list[each][0]])
+                    + '<td>' + list[each][2] + '</td>'
                 ;
+                content += '</tr>';
             };
+            content += '</table>';
         };
 
         outputPage(e, {
             title: '系统状态监视器',
             content
-                : '从这里您可以看到系统的各种状态。' + content + '<br />'
+                : '这里显示的是本系统各个部件的状态。'
+                + '本监视器通过发起IPC（进程间通信）请求，检查本界面系统和各部件的通信是否畅通。'
+                + content
+                + '<br />'
+            ,
+            head
+                : '<style type="text/css">'
+                  + '.report{border: #CCCCCC 1px solid; width: 100%}'
+                  + '.report td{border: #CCCCCC 0.5px solid;}'
+                  + '.report .head{background: #CCCCCC;}'
+                  + '.report .switch{font-weight: bold; text-align: center}'
+                  + '.report .good{background: #00C000; color: #FFFFFF}'
+                  + '.report .error{background: #BB0000; color: #FFFF00}'
+                  + '.report .unknow{background: #FFDD00; color: #FF0000}'
+                + '</style>'
             ,
         });
     };
