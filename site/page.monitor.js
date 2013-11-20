160c7b895ce1696f419a9410b1c9cb1e08e24168
NeoAtlantis <rijndael@yeah.net>
2013-11-20 20:48:54 +0800

    test push via proxy

--- site/page.monitor.js
+++ site/page.monitor.js
@@ -1,8 +1,16 @@
+function queryState(callback){
+    
+};
+
 module.exports = function(e, matchResult){
-    outputPage(e, {
-        title: '系统状态监视器',
-        content
-            : '从这里您可以看到系统的各种状态。<br />'
-        ,
-    });
+    function callback(states){
+        outputPage(e, {
+            title: '系统状态监视器',
+            content
+                : '从这里您可以看到系统的各种状态。<br />'
+            ,
+        });
+    };
+
+    queryState(callback);
 };
