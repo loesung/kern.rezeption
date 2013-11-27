ab891cc9622870148d9a91a2cae4566dcbede09c
NeoAtlantis <rijndael@yeah.net>
2013-11-27 15:55:41 +0800

    fix to rss

--- site/entrance.js
+++ site/entrance.js
@@ -42,7 +42,7 @@ outputPage = function(e, data){
 
     var sidebarWidth = 120;
     var uptime = _.format.seconds2Human($.process.uptime());
-    var memoryUsage = _.format.bytes2Human($.process.memoryUsage().heapTotal);
+    var memoryUsage = _.format.bytes2Human($.process.memoryUsage().rss);
 
     var output    
         = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
