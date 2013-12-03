2558dc99dca3fea64aefd7a093b9fd7a8fbf5766
NeoAtlantis <rijndael@yeah.net>
2013-12-03 13:22:56 +0800

    doing

--- site/page.contact.js
+++ site/page.contact.js
@@ -1,14 +1,49 @@
 var handlers = {};
 
-handlers.list = function(identity, callback){
+handlers.get = function(identity, callback){
     $.nodejs.async.waterfall([
         identity.list,
 
         function(json, callback){
-            callback(null, JSON.stringify(json));
+            callback(null, tablize(json));
         },
 
     ], callback);
+};
+
+function tablize(list, showPage){
+    list.sort(function(a,b){
+        return a.name > b.name;
+    });
+
+    var perPage = 10,
+        maxPage = Math.ceil(list.length / perPage);
+
+    if(isNaN(showPage))
+        showPage = 1;
+    else {
+        showPage = Math.floor(showPage);
+        if(showPage < 1) showPage = 1;
+        if(showPage > maxPage) showPage = maxPage;
+    }
+
+    var ret = '<table class="report" cellspacing="0px" cellpadding="1px">'
+        + '<tr class="head">' 
+            + '<td width="30%">识别ID</td>'
+            + '<td>名称</td>'
+            + '<td width="20%">操作</td>'
+        + '</tr>'
+    for(var i in list){
+        ret += '<tr>'
+            + '<td>' + list[i].id + '</td>'
+            + '<td>' + list[i].name + '</td>'
+            + '<td>' + '删除' + '</td>'
+            + '</tr>'
+        ;
+    };
+    ret += '</table>';
+console.log(ret);
+    return ret;
 };
 
 module.exports = function(e, matchResult, rueckruf){
@@ -26,11 +61,17 @@ module.exports = function(e, matchResult, rueckruf){
                 + '<br />'
                 + content
             ,
+            head
+                : '<style type="text/css">'
+                  + '.report{border: #CCCCCC 1px solid; width: 100%}'
+                  + '.report td{border: #CCCCCC 0.5px solid;}'
+                  + '.report .head{background: #CCCCCC;}'
+                + '</style>'
         });
     };
 
     if(e.method == 'post'){
     } else {
-        handlers.list(identity, respond);
+        handlers.get(identity, respond);
     };
 };
