4d896b5c11732d5d697f062b15386067865941d3
NeoAtlantis <rijndael@yeah.net>
2013-12-03 22:42:29 +0800

    doing contact

--- site/page.contact.js
+++ site/page.contact.js
@@ -22,8 +22,12 @@ handlers.detail = function(identity, post, callback){
         },
 
         function(json, callback){
-            identityInfo = json;
-            callback(null);
+            if(null == json){
+                callback(true);
+            } else {
+                identityInfo = json;
+                callback(null);
+            };
         },
 
         function(callback){
@@ -32,7 +36,85 @@ handlers.detail = function(identity, post, callback){
         },
 
     ], function(err, result){
-        callback(null, 'building');
+        if(null != err){
+            callback(null, '错误：找不到对应的项目。可能是项目已经被删除。');
+            return;
+        };
+
+
+        function prettyID(id){
+            id = id.trim().toUpperCase();
+            var breaked = [];
+            var color = 0;
+            while(id != '' && color < 100){
+                breaked.push((
+                    (color == 0?'<font color="#FF0000">':'') + 
+                    id.substr(0,4) + 
+                    (color == 3?'</font>':'')
+                ));
+                color += 1;
+                id = id.substr(4);
+            };
+            var line = [];
+            var output = [];
+            for(var i in breaked){
+                line.push(breaked[i]);
+                if(line.length >= 8){
+                    output.push(line.join(' '));
+                    line = [];
+                };
+            };
+            if(line.length > 0) output.push(line.join(' '));
+            return output.join('<br />');
+        };
+        function breakBlock(block, width){
+            var output = '';
+            var counter = 0;
+            while(block != ''){
+                output += block.substr(0,1);
+                block = block.substr(1);
+                counter += 1;
+                if(counter == width){
+                    output += '<br />';
+                    counter = 0;
+                };
+            };
+            output += block;
+            return output;
+        };
+        var output = ''
+            + '<table class="report" cellpadding="0" cellspacing="0">'
+            +   '<tr class="head">'
+            +       '<td>全名</td>'
+            +       '<td width="55%">识别ID</td>'
+            +   '</tr>'
+            +   '<tr>'
+            +       '<td>' + breakBlock(identityInfo.name, 32) + '</td>'
+            +       '<td>' + prettyID(identityInfo.id) + '</td>'
+            +   '</tr>'
+            +   '<tr class="head">'
+            +       '<td colspan="2">'
+            +           '<table class="fineLine" cellspacing="0" cellpadding="0">'
+            +               '<form method="POST" action="/' + (new Date().getTime()) + '/contact/remove">' 
+            +                   '<input type="hidden" value="' + identityInfo.id + '">' 
+            +                   '<tr>'
+            +                       '<td>删除：请抄写识别ID前16位字母 <input type="text" name="confirm" size="17" maxlength="16"/></td>'
+            +                       '<td><button type="submit">确定删除</button></td>'
+            +                   '</tr>'
+            +              '</form>'
+            +              '<form method="GET" action="/' + (new Date().getTime()) + '/codebook/' + identityInfo.id + '">' 
+            +                   '<tr>'
+            +                       '<td>要查看、新增或管理本机用于和此联系人进行机密通信的密码本，请点击：</td>'
+            +                       '<td><button type="submit">管理密码本</button></tr>'
+            +                  '</tr>'
+            +              '</form>'
+            +           '</table>'
+            +       '</td>'
+            +   '</tr>'
+
+            + '</table>'
+        ;
+        callback(null, output);
     });
 };
 
@@ -89,24 +171,23 @@ function tablize(list, showPage){
 module.exports = function(e, matchResult, rueckruf){
     var identity = _.identity(IPC['datenbank']);
     var subcommand = matchResult[3];
+    var subtitle = false;
 
     function respond(err, content){
         if(null != err){
             content = '错误：无法连接到数据中心。';
         };
         outputPage(e, {
-            title: '联系人',
-            content
-                : '' 
-                + '<br />'
-                + content
-            ,
+            title: '联系人' + (subtitle?' > ' + subtitle:''),
+            content: content,
             head
                 : '<style type="text/css">'
+                  + 'input[type="hidden"]{display: none}'
+                  + 'td{word-break:break-all}'
                   + '.report{border: #CCCCCC 1px solid; width: 100%}'
                   + '.report td{border: #CCCCCC 0.5px solid;}'
                   + '.report .head{background: #CCCCCC;}'
-                  + '.report button{font-size: 9pt; text-align: center; background: #FFAAAA;}'
+                  + '.report button,input{font-size: 9pt; text-align: center; background: #FFAAAA;}'
                 + '</style>'
         });
     };
@@ -119,6 +200,7 @@ module.exports = function(e, matchResult, rueckruf){
         };
         switch(subcommand){
             case 'detail':
+                subtitle = "详细信息";
                 waitAndRespond(handlers.detail);
                 break;
             default:
--- site/router.js
+++ site/router.js
@@ -4,6 +4,7 @@ var routerTable = {
     '^(\/[0-9]+)?\/compose\/?\\??$': require('./page.compose.js'),
     '^(\/[0-9]+)?\/monitor\/?\\??$': require('./page.monitor.js'),
     '^(\/[0-9]+)?\/contact(\/(detail|add|remove))?\/?\\??$': require('./page.contact.js'),
+    '^(\/[0-9]+)?\/codebook\/([0-9a-fA-F]+)(\/(detail|add|remove))?\/?\\??$': require('./page.contact.js'),
     '^(\/[0-9]+)?\/log\/?\\??$': require('./page.log.js'),
 
     '^\/static\/([0-9a-zA-Z\.\-]+)$': require('./page.static.js'),
