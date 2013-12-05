72125af600fe1bfc39b5d0524f1841dd3e27fd33
NeoAtlantis <rijndael@yeah.net>
2013-12-06 02:21:13 +0800

    doing plaintext work

---
+++ site/page.msgcenter.plaintext.listqueue.js
@@ -0,0 +1,78 @@
+var PER_PAGE = 8;
+module.exports = function(queues, parameter, post, respond){
+    var workflow = [];
+    var pager = null;
+
+    workflow.push(queues.send.pending.list);
+
+    workflow.push(function(list, callback){
+        list.sort(function(a, b){
+            return b.timestamp - a.timestamp;
+        });
+
+        pager = _.paging(list, PER_PAGE, parameter);
+
+        callback(null, pager);
+    });
+
+    workflow.push(function(list, callback){
+        var result = [];
+        for(var i in pager.list){
+            result.push((function(){
+                var itemID = pager.list[i].id;
+                return function(callback){
+                    queues.send.pending.query(itemID, callback);
+                };
+            })());
+        };
+        $.nodejs.async.parallel(result, callback);
+    });
+
+    $.nodejs.async.waterfall(workflow, function(err, result){
+        var output = '<form method="POST" action="/' + (new Date().getTime()) + '/msgcenter/plaintext/-/do">'
+            +'<table class="report" cellspacing="0" cellpadding="0" style="font-size: 9pt">'
+            + '<tr class="head">'
+            +   '<td width="20%">时间</td>'
+            +   '<td>内容</td>'
+            +   '<td width="5%">选择</td>'
+            + '</tr>'
+        ;
+
+        for(var i in result){
+            var item = result[i];
+            output 
+                += '<tr>'
+                +       '<td>' + _.format.time2Full(
+                                    new Date(item.timestamp * 1000)
+                    ) + '</td>'
+                +       '<td>' + item.data + '</td>'
+                +       '<td><input type="checkbox" /></td>'
+                + '</tr>'
+            ;
+        };
+        output += '</table>'
+            + '第' + pager.current + '页/共' + pager.max + '页 '
+            + pager.navigateBar(function(i, cur){
+                if(cur)
+                    return ('[<font color="#FF0000">' + i + '</font>]');
+                else
+                    return ('[<a href="/' + (new Date().getTime())
+                        + '/msgcenter/plaintext/' + i + '">' + i + '</a>]')
+                    ;
+                }
+            )
+            + '<br /><table><tr><td>选中项目：</td>'
+            + '<td><select name="send">'
+            +   '<option value="passphrase" selected>加密，使用临时输入的口令</option>'
+            +   '<option value="codebook">加密，指定一个或多个收件人，使用密码本</option>'
+            +   '<option value="sign">使用公钥签署，但不加密</option>'
+            + '</select></td>'
+            + '<td><button type="submit" name="do" value="send">发送</button></td>'
+            + '<td><button type="submit" name="do" value="remove">删除</button></td>'
+            + '</tr></table>'
+            + '</form>'
+        ;
+
+        respond(null, output);
+    });
+};
---
+++ site/page.msgcenter.plaintext.process.js
@@ -0,0 +1,21 @@
+module.exports = function(queues, parameter, post, respond){
+    var isID = /^[0-9a-f]{8}\-([0-9a-f]{4}\-){3}[0-9a-f]{12}$/i;
+
+    /* Determine objects being operated
+     * - if by post, read posted body.
+     * - read parameter
+     * Anyway, use isID.test() to filter all id.
+     */
+    var objectIDs = [];
+    if(isID.test(parameter)) objectIDs.push(parameter.toLowerCase());
+    if($.types.isObject(post)){
+        for(var key in post.parsed){
+            if(isID.test(post.parsed[key]))
+                objectIDs.push(post.parsed[key].toLowerCase());
+        };
+    };
+
+
+    console.log(parameter, post);
+    respond(null, 'Hallo');
+};
