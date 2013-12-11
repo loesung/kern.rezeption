96c58a023d23a1b72f075791583ee01f69ddd1d8
NeoAtlantis <rijndael@yeah.net>
2013-12-12 01:03:58 +0800

    add contact

--- site/page.contact.add.js
+++ site/page.contact.add.js
@@ -1,12 +1,24 @@
 module.exports = function(identity, post, callback){
-    console.log(post);
-
     $.nodejs.async.waterfall([
-        identity.list,
-
-        function(json, callback){
-            callback(null, 'I\'m working on it.');
+        function(callback){
+            identity.add(post.parsed.name, callback);
         },
 
-    ], callback);
+        function(json, callback){
+            callback(null, json);
+        },
+
+    ], function(err, result){
+        if(null == err){
+            callback(true, '添加成功。');
+            return;
+        };
+
+        if(true === err) {
+            callback(true, '添加失败，名称不合适，或者该联系人已经存在。');
+            return;
+        };
+
+        callback(true, '添加失败，错误代码为：' + err);
+    });
 };
