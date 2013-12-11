ca1cc4e82f6a0a664d36afbc79e8f5b9fd0b7410
NeoAtlantis <rijndael@yeah.net>
2013-12-12 02:05:25 +0800

    begin to fill

---
+++ site/page.msgcenter.ciphertext.js
@@ -0,0 +1,13 @@
+var handlers = {
+    listQueue: require('./page.msgcenter.encrypted.listqueue.js'),
+    process: require('./page.msgcenter.encrypted.process.js'),
+};
+
+
+module.exports = function(queues, parameter, action, post, respond){
+    if($.types.isString(action)){
+        handlers.process(queues, parameter, post, respond, action);
+    } else {
+        handlers.listQueue(queues, parameter, post, respond);
+    };
+};
---
+++ site/page.msgcenter.decrypted.js
@@ -0,0 +1,13 @@
+var handlers = {
+    listQueue: require('./page.msgcenter.plaintext.listqueue.js'),
+    process: require('./page.msgcenter.plaintext.process.js'),
+};
+
+
+module.exports = function(queues, parameter, action, post, respond){
+    if($.types.isString(action)){
+        handlers.process(queues, parameter, post, respond, action);
+    } else {
+        handlers.listQueue(queues, parameter, post, respond);
+    };
+};
