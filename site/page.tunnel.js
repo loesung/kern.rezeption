01f33088ad90b65de574338ce107bea79ffd127e
NeoAtlantis <rijndael@yeah.net>
2013-12-17 16:35:18 +0800

    list tunnels

--- site/page.tunnel.js
+++ site/page.tunnel.js
@@ -7,7 +7,50 @@ function statistics(callback){
 };
 
 function showReceiver(receiverID, callback){
-    callback(null, 
+    receiverID = receiverID.substr(0,16).toUpperCase();
+    var tunnels = $.global.get('botschaft').receiverTunnels(receiverID),
+        content
+    ;
+
+    if(null == tunnels){
+        content = '当前尚未在通讯系统中找到为ID <font color="#FF0000">'
+            + receiverID
+            + '</font> 提供的通讯信道。'
+            + '<br />请检查通讯系统设置和启动情况，然后刷新页面。'
+        ;
+    } else {
+        content = '下面列出通讯系统为您提供，与用户 <font color="#FF0000">' 
+            + receiverID + '</font> 联系所用的信道。'
+            + '<table class="report" cellspacing="0px" cellpadding="3px">'
+            + '<tr class="head">'
+            + '<td width="10%">协议</td>'
+            + '<td>说明</td>'
+            + '</tr>'
+        ;
+
+        var listed = tunnels.list(), tunnelID;
+        for(var tunnelID in listed){
+            idInfo = listed[tunnelID];
+            tunnelInfo = tunnels.info(tunnelID);
+            if(!tunnelInfo) continue;
+
+            content += '<tr><td>';
+
+            if(/^(xmpp)$/.test(idInfo.protocol))
+                content += '<img src="/static/protocol.'
+                    + idInfo.protocol + '.png"></img>';
+
+            content += '</td>'
+                + '<td>' + tunnelInfo.description + '</td>'
+                + '</tr>'
+            ;
+        };
+
+        content += '</table>';
+    };
+
+    callback(null, content, '详细信息');
+    /*
         '<table class="report" cellspacing="0px" cellpadding="3px">'
         + '<tr class="head">'
         + '<td width="20%">项目</td>'
@@ -16,7 +59,7 @@ function showReceiver(receiverID, callback){
         + '</tr>'
         + '</table>',
         '详细信息'
-    );
+    );*/
 };
 
 module.exports = function(e, matchResult, rueckruf){
